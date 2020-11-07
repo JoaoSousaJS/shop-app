4a2d5d0b7e1848aec98530f3717d8484909a3728
Joao <joao.rsousa23@gmail.com>
2020-11-07 17:37:57 +1030

    feat: add font to title and description

--- src/navigation/ScreenOptions/screen-options.ts
+++ src/navigation/ScreenOptions/screen-options.ts
@@ -5,5 +5,11 @@ export const defaultNavigationOptions = {
   headerStyle: {
     backgroundColor: Platform.OS === 'android' ? AppColors.primary : 'white',
   },
+  headerTitleStyle: {
+    fontFamily: 'open-sans-bold',
+  },
+  headerBackTitleStyle: {
+    fontFamily: 'open-sans',
+  },
   headerTintColor: Platform.OS === 'android' ? 'white' : AppColors.primary,
 };
