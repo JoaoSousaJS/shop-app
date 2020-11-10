5b53795dfa08be24096e2e9c02dfe2f991d17b80
Joao <joao.rsousa23@gmail.com>
2020-11-11 09:42:42 +1030

    feat: add cart reducer and actions

---
+++ src/store/reducers/cart.ts
@@ -0,0 +1,42 @@
+import { CartItem } from '../../models/card-item';
+import { Product } from '../../models/products';
+import { ADD_TO_CART } from '../actions/cart';
+
+const initialState = {
+  items: {},
+  totalAmount: 0,
+};
+
+interface IAction {
+  type: string;
+  product: Product;
+}
+
+export default (state = initialState, action: IAction) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const addedProduct = action.product;
+      const prodPrice = addedProduct.price;
+      const prodtTitle = addedProduct.title;
+
+      let updateOrNewCartItem;
+
+      if (state.items[addedProduct.id]) {
+        // already have the item in the cart
+        updateOrNewCartItem = new CartItem(
+          state.items[addedProduct.id].quantity + 1,
+          prodPrice,
+          prodtTitle,
+          state.items[addedProduct.id].sum + prodPrice,
+        );
+      } else {
+        updateOrNewCartItem = new CartItem(1, prodPrice, prodtTitle, prodPrice);
+      }
+      return {
+        ...state,
+        items: { ...state.items, [addedProduct.id]: updateOrNewCartItem },
+        totalAmount: state.totalAmount + prodPrice,
+      };
+  }
+  return state;
+};
