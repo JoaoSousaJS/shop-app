37a1e908ba1a6e29168b48b99b07c0eef48e9ad7
Joao <joao.rsousa23@gmail.com>
2020-11-11 18:39:58 +1030

    feat: add cart icon to product overview screen

--- src/navigation/ShopNavigator.tsx
+++ src/navigation/ShopNavigator.tsx
@@ -1,9 +1,12 @@
-import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
+import { Platform, TouchableOpacity } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { ProductDetailScreen } from '../screens/shop/ProductDetailScreen';
 import { ProductsOverviewScreen } from '../screens/shop/ProductsOverviewScreen';
 import { defaultNavigationOptions } from './ScreenOptions/screen-options';
 import { ProductOverviewStackParamList } from './types/ProductOverview/RouteParamList';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { CustomHeaderButton } from '../components/UI/HeaderButton';
 
 const Stack = createStackNavigator<ProductOverviewStackParamList>();
 
@@ -13,7 +16,20 @@ export const ShopStackNavigator = () => {
       <Stack.Screen
         name="productsOverview"
         component={ProductsOverviewScreen}
-        options={{ title: 'All Products' }}
+        options={{
+          title: 'All Products',
+          headerRight: () => (
+            <TouchableOpacity>
+              <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+                <Item
+                  title="Cart"
+                  iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+                  onPress={() => {}}
+                />
+              </HeaderButtons>
+            </TouchableOpacity>
+          ),
+        }}
       />
       <Stack.Screen
         name="productsDetails"
