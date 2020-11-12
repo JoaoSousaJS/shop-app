894caaeecf5ca9009b01e7dacf237b7cfc107edd
Joao <joao.rsousa23@gmail.com>
2020-11-12 21:05:41 +1030

    feat: add list to OrderScreen

--- src/models/orders.ts
+++ src/models/orders.ts
@@ -9,4 +9,14 @@ export class Order {
     this.totalAmount = totalAmount;
     this.date = date;
   }
+
+  get readableDate() {
+    return this.date.toLocaleDateString('en-EN', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+  }
 }
--- src/screens/shop/OrdersScreen.tsx
+++ src/screens/shop/OrdersScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList } from 'react-native';
 import { useSelector } from 'react-redux';
+import { OrderItem } from '../../components/shop/OrderItem';
 import { IOrderState } from '../../store/types/order';
 
 export const OrdersScreen = () => {
@@ -9,7 +10,12 @@ export const OrdersScreen = () => {
   return (
     <FlatList
       data={orders}
-      renderItem={(itemData) => <Text>{itemData.item.totalAmount}</Text>}
+      renderItem={(itemData) => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.readableDate}
+        />
+      )}
     />
   );
 };
